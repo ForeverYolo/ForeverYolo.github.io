818eb40f68380cac9858dcb8539335b4afa4d2cf
UnicornQi <2572131118@qq.com>
2022-12-29 12:13:06 +0800

    Site updated: 2022-12-29 12:13:04

--- js/foot.js
+++ js/foot.js
@@ -1,6 +1,6 @@
 // 动态心跳，更改自己的名称
 $(document).ready(function(e){
-    $('.copyright').html('<font style=color:#1E90FF>©2022 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By <font style=color:#FF00FF>ForverYolo');
+    $('.copyright').html('<font style=color:#1E90FF>©2022 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By <font style=color:#FF00FF>ForeverYolo');
 })
 
 $(document).ready(function(e){
